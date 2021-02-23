bce66543a40b8cc90ecb7557b736a65fa2c7d60d
Universal Studio <uni@un1.pw>
2021-02-23 15:38:03 +0100

    fix: typescript definitions

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,9 @@
-declare module "acronymresolver" {
-  /**
-   * Acronym Resolver
-   *
-   * Resolve acronyms randomly
-   * @param {String} input An acronym (e.g. SMS, BBC)
-   * @returns {String} randomly resolved acronym
-   */
-  export default function acronyms(input: string): string
-}
+/**
+ * Acronym Resolver
+ *
+ * Resolve acronyms randomly
+ * @param {String} input An acronym (e.g. SMS, BBC)
+ * @returns {String} randomly resolved acronym
+ */
+declare function acronyms(input: string): string
+export = acronyms
